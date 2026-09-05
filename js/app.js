7016d99a07fb9d1cc0fe5bc5a1e2c80cd1947385
runny-life(Nikita) <runny_life@icloud.com>
2026-09-05 10:30:56 +0300

    feat: add animation open faq accordion

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ const detailsElements = document.querySelectorAll(".faq__details");
 
 detailsElements.forEach(detail => {
   detail.addEventListener("toggle", function () {
-    if(this.open) {
+    if (this.open) {
+      this.classList.add("is-open");
+
       detailsElements.forEach(other => {
-        if(other !== this && other.open) {
+        if (other !== this && other.open) {
           other.open = false;
+          other.classList.remove("is-open");
         }
-      })
+      });
+    } else {
+      this.classList.remove("is-open");
     }
   })
 })
